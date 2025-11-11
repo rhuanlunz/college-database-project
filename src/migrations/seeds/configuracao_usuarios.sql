a87047dcb88c1dfcdb6bd0babed253e468b5c36f
rhuanlunz <rhuan@C0V1L>
2025-11-11 14:41:23 -0300

    fix: removed invalid char

---
+++ src/migrations/seeds/configuracao_usuarios.sql
@@ -0,0 +1,101 @@
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(1, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(2, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(3, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(4, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(5, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(6, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(7, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(8, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(9, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(10, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(11, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(12, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(13, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(14, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(15, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(16, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
+
+INSERT INTO configuracao_usuarios
+(id_usuario, perfil_privado, modo_escuro, notificacoes, mostrar_status_online,
+ permitir_novos_seguidores, permitir_mensagens_privadas, filtro_conteudo, idioma)
+VALUES
+(17, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, 'pt-BR');
