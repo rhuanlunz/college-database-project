6c358e6e3d53ec03a97f6c0c0bc3ad8e8064b999
rhuanlunz <rhuan@C0V1L>
2025-11-11 11:24:04 -0300

    style: changed messages language

--- migrations/seeds/log_atividades_usuarios.sql
+++ migrations/seeds/log_atividades_usuarios.sql
@@ -2,43 +2,40 @@ INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES
     (1, 'login');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (2, 'post created');
+    (2, 'criou um novo post');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (3, 'commented');
+    (3, 'criou um novo comentario');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (4, 'liked post');
+    (4, 'curtiu um post');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (5, 'logged out');
+    (5, 'logout');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (6, 'updated profile');
+    (6, 'atualizou seu perfil');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (7, 'changed settings');
+    (7, 'atualizou suas configuracoes');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (8, 'uploaded photo');
+    (8, 'seguiu um usuario');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (9, 'followed user');
+    (9, 'parou de seguir um usuario');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (10, 'unfollowed user');
+    (10, 'criou um novo post');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (11, 'post created');
+    (11, 'criou um novo comentario');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (12, 'commented');
+    (12, 'curtiu um post');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (13, 'liked post');
+    (13, 'login');
     
 INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (14, 'login');
-    
-INSERT INTO log_atividades_usuarios (id_usuario, acao_realizada) VALUES  
-    (15, 'logout');
+    (14, 'logout');
